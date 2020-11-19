5f3fdfdde1307f9a9ae485806b844b3a8d8fd5ca
TrikThom <thomas@trikthom.com>
2020-11-19 16:24:48 +0100

    Updated WebSocket library

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,24 @@
 #include <websocket_server.h>
 
+#include <string>
+#include <iostream>
+
 int main() {
-  
+
   WebSocket::Server server;
+
+  server.on_connection = [](WebSocket::Connection * connection) {
+    std::cout << "new connection" << std::endl;
+
+    uint8_t buffer[] = { 0x81, 0x0C, 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
+    connection->write(buffer, sizeof(buffer));
+
+    connection->on_message = [connection](std::string message) {
+      std::cout << message << std::endl;
+      uint8_t buffer[] = { 0x81, 0x0D, 'D', 'e', 'f', 'a', 'u', 'l', 't', ' ', 'r', 'e', 'p', 'l', 'y' };
+      connection->write(buffer, sizeof(buffer));
+    };
+  };
 
   server.listen(3000);
 
