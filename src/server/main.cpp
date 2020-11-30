dc6e3c7ef402722a37d00243284f076940df7f45
TrikThom <thomas@trikthom.com>
2020-11-30 16:10:49 +0100

    Fixed connections not updating

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -17,13 +17,12 @@ int main() {
 
   server.set_debug(true);
 
-  std::vector<WebSocket::Connection *> connections;
+  static std::vector<WebSocket::Connection *> connections;
 
   server.on_connection = [&](WebSocket::Connection * connection) {
-    connection->write("Hello World!");
     connections.push_back(connection);
 
-    connection->on_message = [connection, connections](std::string message) {
+    connection->on_message = [connection](std::string message) {
       std::cout << message << std::endl;
       for (WebSocket::Connection * con : connections) {
         if (con->is_connected() && con != connection) con->write(trim_string(message));
